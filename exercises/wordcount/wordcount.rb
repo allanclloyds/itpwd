f67a86b6305dfb82063775cb15e10dfd85db7c4b
Allan C. Lloyds <acl@acl.im>
2012-04-23 14:37:06 +0200

    New exercise: wordcount

---
+++ exercises/wordcount/wordcount.rb
@@ -0,0 +1,39 @@
+# Problem:
+#
+#   Count number of words in text
+#
+# Display like:
+#
+#   word1: 12
+#   word2: 8
+#   word3: 2
+#
+#   total: x
+#
+# Remember: You should have two solutions: 1 pseudocode, 1 ruby
+
+while line = DATA.gets
+  puts line
+end
+
+__END__
+
+I remember him as if it were yesterday, as he came plodding to the inn door,
+his sea-chest following behind him in a hand-barrow—a tall, strong, heavy,
+nut-brown man, his tarry pigtail falling over the shoulder of his soiled blue
+coat, his hands ragged and scarred, with black, broken nails, and the sabre cut
+across one cheek, a dirty, livid white. I remember him looking round the cover
+and whistling to himself as he did so, and then breaking out in that old
+sea-song that he sang so often afterwards:
+
+          "Fifteen men on the dead man's chest -
+             Yo-ho-ho, and a bottle of rum!"
+
+in the high, old tottering voice that seemed to have been tuned and broken at
+the capstan bars. Then he rapped on the door with a bit of stick like a
+handspike that he carried, and when my father appeared, called roughly for a
+glass of rum. This, when it was brought to him, he drank slowly, like a
+connoisseur, lingering on the taste and still looking about him at the cliffs
+and up at our signboard.
+
+  -- Robert Louis Stevenson
